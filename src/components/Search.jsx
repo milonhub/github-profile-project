620b1c887260685e9e3e84ed50e8cdcd75066e57
milonhub <milonru150@gmail.com>
2023-11-14 11:48:05 -0800

    m

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,10 +3,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 const Search = () => {
 
+  const handleSearchData = (event) =>{
+        console.log(event.target.value)
+        
+  }
+
   return (
          <div className=' bg-slate-500 rounded-xl'>
           <FontAwesomeIcon icon= {faMagnifyingGlass} color='white' fontSize="40px" className='pl-4'/>
-          <input placeholder="User name" className="text-4xl p-5 w-[600px] outline-none   bg-slate-500 text-stone-50">
+          <input placeholder="User name" className="text-4xl p-5 w-[600px] outline-none   bg-slate-500 text-stone-50" onChange={handleSearchData}>
           </input>
          
          </div>
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -14,25 +14,26 @@ const Hero = () => {
             <Search />
         </div>
        
-        <div className="h-full" style={{background: "#20293A", height: "80vh"}} >
+        <div style={{background: "#20293A", height: "100%"}} >
           <div>
           <div className="absolute left-[10%] top-[285px]">
           <FontAwesomeIcon icon= {faSquareGithub} color="white" className="text-[150px]"/>
           </div>
-          <div className="flex gap-16 justify-center relative top-5">
-          <div className="flex gap-4 bg-slate-950 p-3 rounded-xl">
+          
+          <div className="laptop:flex laptop:flex-row laptop:gap-16 desktop:flex-row desktop:flex desktop:gap-16 justify-center relative top-5 mobile:flex mobile:flex-col">
+          <div className="flex gap-4 bg-slate-950 p-3 rounded-xl w-72">
              <span className="text-3xl text-white p-2">Follwers</span>
-             <div className="h-16 w-[1px] bg-white"></div>
+             <div className="h-16 w-[1px] bg-white"> </div>
              <span className="text-white text-3xl p-2">278356</span>
 
           </div>
-          <div className="flex gap-4 bg-slate-950 p-3 rounded-xl">
+          <div className="flex gap-4 bg-slate-950 p-3 rounded-xl w-56">
              <span className="text-3xl text-white p-2">Follwing</span>
              <div className="h-16 w-[1px] bg-white"></div>
              <span className="text-white text-3xl p-2">0</span>
 
           </div>
-          <div className="flex gap-4 bg-slate-950 p-3 rounded-xl">
+          <div className="flex gap-4 bg-slate-950 p-3 rounded-xl w-[450px]">
           <span className="text-3xl text-white p-2">Location</span>
              <div className="h-16 w-[1px] bg-white"></div>
              <span className="text-white text-3xl p-2">San Fransica, CA</span>
@@ -43,13 +44,13 @@ const Hero = () => {
             <h1 className="text-6xl leading-normal">GitHub</h1>
             <p className="text-3xl">How people build software</p>
           </div>
-          <div className="flex gap-5 flex-wrap my-10">
+          <div className="grid grid-cols-2 gap-20 my-9">
 
-          <div className="text-white bg-slate-950 p-8 w-[650px] h-[250px] rounded-xl">
+          <div className="text-whitep-8 rounded-xl p-8 text-white" style={{backgroundColor: "#1B1A42"}}>
                 <span className="text-4xl">.github</span>
                 <p className="text-xl my-3">Community health files for the @Github organizatioon</p>
-                <div className="flex gap-12 my-4">
-                <div>
+                <div className="flex flex-wrap gap-12 my-4">
+                <div className="flex">
                   <FontAwesomeIcon icon={faCodeBranch} fontSize="24px" />
                   <span className="text-2xl mx-4">2345</span>
                   </div>
@@ -57,17 +58,18 @@ const Hero = () => {
                    <div>
                     <FontAwesomeIcon icon={faStar} fontSize="24px"/>
                     <span className="text-2xl mx-4">703</span>
-                   </div>
+                    </div>
                   <p className="text-2xl">updated 4days ago</p>
+                  
                 </div>
             </div>
 
 
           
-            <div className="text-white bg-slate-950 p-8 w-[650px] h-[250px] rounded-xl">
-                <span className="text-4xl">.github</span>
-                <p className="text-xl my-3">Community health files for the @Github organizatioon</p>
-                <div className="flex gap-12 my-4">
+            <div className="text-white bg-slate-950 p-8 rounded-xl">
+                <span className="text-4xl">accesibility-alt-text-bot</span>
+                <p className="text-xl my-3">an action to remind users to add alt text on issues. Pull requests and discussion</p>
+                <div className="flex flex-wrap gap-12 my-4">
                 <div>
                   <FontAwesomeIcon icon={faCodeBranch} fontSize="24px" />
                   <span className="text-2xl mx-4">2345</span>
@@ -83,13 +85,11 @@ const Hero = () => {
 
 
        
-
-
             <div>
-            <div className="text-white bg-slate-950 p-8 w-[650px] h-[250px] rounded-xl">
-                <span className="text-4xl">.github</span>
-                <p className="text-xl my-3">Community health files for the @Github organizatioon</p>
-                <div className="flex gap-12 my-4">
+            <div className="text-white p-8 rounded-xl" style={{backgroundColor: "#1B1A42"}}>
+                <span className="text-4xl">accesibilityjs</span>
+                <p className="text-xl my-3">Client Side accesibility error scanner</p>
+                <div className="flex flex-wrap gap-12 my-4">
                 <div>
                   <FontAwesomeIcon icon={faCodeBranch} fontSize="24px" />
                   <span className="text-2xl mx-4">2345</span>
@@ -107,10 +107,10 @@ const Hero = () => {
             </div>
 
             <div>
-            <div className="text-white bg-slate-950 p-8 w-[650px] h-[250px] rounded-xl">
-                <span className="text-4xl">.github</span>
-                <p className="text-xl my-3">Community health files for the @Github organizatioon</p>
-                <div className="flex gap-12 my-4">
+            <div className="text-white bg-slate-950 p-8 rounded-xl">
+                <span className="text-4xl">Actions-cheat-sheet</span>
+                <p className="text-xl my-3">A cheat sheet for Github Actions</p>
+                <div className="flex flex-wrap gap-12 my-4">
                 <div>
                   <FontAwesomeIcon icon={faCodeBranch} fontSize="24px" />
                   <span className="text-2xl mx-4">2345</span>
@@ -130,11 +130,13 @@ const Hero = () => {
          </div>
         
         
+        
         </div>
-      
+        <p className="text-3xl text-white text-center py-8">View all respository</p>
     </div>
+   
   </div>
-
+  
     </div>
   )
 }
